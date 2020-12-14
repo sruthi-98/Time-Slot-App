957b670e009d297e8a4cc15826db086a2497acb1
Sruthi Suresh <sruthisuresh321@gmail.com>
2020-12-14 21:49:38 +0530

    Add function to return date in DD-Month-YYYY format

--- src/components/TImeSlotList.js
+++ src/components/TImeSlotList.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
 function TImeSlotList() {
+    const getTodayDate = () => {
+        const date = new Date();
+        const day = date.getDate();
+        const month = date.toLocaleString('default', { month: 'long' });
+        const year = date.getFullYear();
+        return `${day} ${month} ${year}`;
+    }
+
     return (
         <div>
-            
+            <h1>{getTodayDate()}</h1>
         </div>
     )
 }
