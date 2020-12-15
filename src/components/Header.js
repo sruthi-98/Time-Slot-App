14da2b4427a1f7aa6897903e0d61df8dbcfabd12
Sruthi Suresh <sruthisuresh321@gmail.com>
2020-12-15 13:01:18 +0530

    Add styling to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 function Header() {
     return (
-        <div className="Header">
-            <h3>Time Slot App</h3>
+        <div className="p-4 bg-yellow-300">
+            <h3 className="text-2xl text-white font-extrabold uppercase mx-4">Time Slot App</h3>
         </div>
     )
 }
