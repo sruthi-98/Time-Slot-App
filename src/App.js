a9c6f4cf815099ab509ac8efe7d67ef3137df964
Sruthi Suresh <sruthisuresh321@gmail.com>
2020-12-15 08:42:03 +0530

    Include form component and add routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Form from './components/Form';
 import Header from './components/Header';
 import TImeSlotList from './components/TImeSlotList';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <TImeSlotList />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path='/form/:slot'>
+            <Header />
+            <Form />
+          </Route>
+          <Route path='/'>
+            <Header />
+            <TImeSlotList />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
