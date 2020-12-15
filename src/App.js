7b8029d139d5cd210abd6c6c7a138b62d39eb5e3
Sruthi Suresh <sruthisuresh321@gmail.com>
2020-12-15 08:14:27 +0530

    Include header and time slot component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import Header from './components/Header';
+import TImeSlotList from './components/TImeSlotList';
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <TImeSlotList />
     </div>
   );
 }
