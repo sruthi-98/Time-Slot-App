20df7efb6aeda795eb9d464212b80a516059b6f2
Sruthi Suresh <sruthisuresh321@gmail.com>
2020-12-15 14:44:01 +0530

    Fix typo

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Form from './components/Form';
 import Header from './components/Header';
-import TImeSlotList from './components/TImeSlotList';
+import TimeSlotList from './components/TimeSlotList';
 
 function App() {
   return (
@@ -15,7 +15,7 @@ function App() {
           </Route>
           <Route path='/'>
             <Header />
-            <TImeSlotList />
+            <TimeSlotList />
           </Route>
         </Switch>
       </div>
