d54f52bbe9a14dc0bffbfca1a4fbdbeb3316face
Sruthi Suresh <sruthisuresh321@gmail.com>
2020-12-15 08:43:58 +0530

    Add form comprising first name, last name and phone number

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,7 +3,28 @@ import React from 'react';
 function Form() {
     return (
         <div>
-            
+            <h1>{slot}</h1>
+            <form>
+                <div>
+                    <label>First Name</label>
+                    <input type="text" name="" id=""/>
+                </div>
+
+                <div>
+                    <label>Last Name</label>
+                    <input type="text" name="" id=""/>
+                </div>
+
+                <div>
+                    <label>Phone Number</label>
+                    <input type="number" name="" id=""/>
+                </div>
+
+                <div>
+                    <button type="reset">Cancel</button>
+                    <button type="submit">Submit</button>
+                </div>
+            </form>
         </div>
     )
 }
