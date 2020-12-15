e87ffc56b2081eba23eea43f1164c6d8a735ac37
Sruthi Suresh <sruthisuresh321@gmail.com>
2020-12-15 08:13:33 +0530

    Create function for generating slots

--- src/components/TImeSlotList.js
+++ src/components/TImeSlotList.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
 function TImeSlotList() {
+    // Hard coded timeslot array
+    const timeSlots = ['9 am - 10 am', '10 am - 11 am', '11 am - 12 pm', '12 pm - 1 pm', '1 pm - 2 pm', '2 pm - 3 pm', '3 pm - 4 pm', '4 pm - 5 pm'];
+
     // Returns date in DD-Month-YYYY format
     const getTodayDate = () => {
         const date = new Date();
@@ -10,12 +13,19 @@ function TImeSlotList() {
         return `${day} ${month} ${year}`;
     }
 
-    // Hard coded timeslot array
-    const timeSlots = ['9 am', '10 am', '11 am', '12 pm', '1 pm', '2 pm', '3 pm', '4 pm', '5 pm'];
+    const generateSlots = (timeSlots) => {
+        const slots = timeSlots.map(slot => {
+            return (
+                <li>{slot}</li>
+            )
+        });
+        return slots;
+    }
     
     return (
         <div>
             <h1>{getTodayDate()}</h1>
+            <ul>{generateSlots(timeSlots)}</ul>
         </div>
     )
 }
