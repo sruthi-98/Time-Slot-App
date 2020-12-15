42d86e405e59c8af755f3ca7b4883b2230711bc1
Sruthi Suresh <sruthisuresh321@gmail.com>
2020-12-15 10:32:45 +0530

    Create state for form fields

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams, useHistory } from 'react-router-dom';
 
 function Form() {
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [phoneNumber, setPhoneNumber] = useState('');
     const { slot } = useParams();
     const history = useHistory();
 
@@ -11,17 +14,29 @@ function Form() {
             <form>
                 <div>
                     <label>First Name</label>
-                    <input type="text" name="" id=""/>
+                    <input 
+                        type="text"
+                        value={firstName}
+                        onChange={e => setFirstName(e.target.value)}
+                    />
                 </div>
 
                 <div>
                     <label>Last Name</label>
-                    <input type="text" name="" id=""/>
+                    <input 
+                        type="text"
+                        value={lastName}
+                        onChange={e => setLastName(e.target.value)}
+                    />
                 </div>
 
                 <div>
                     <label>Phone Number</label>
-                    <input type="tel" name="" id=""/>
+                    <input 
+                        type="text"
+                        value={phoneNumber}
+                        onChange={e => setPhoneNumber(e.target.value)}
+                    />
                 </div>
 
                 <div>
